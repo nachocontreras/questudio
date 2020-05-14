c39ada944f45c671806408b8d489900ca4bd744b
Luciano <lldavico@uc.cl>
2020-05-14 10:51:13 -0400

    experience permissions

--- src/routes/middlewares.js
+++ src/routes/middlewares.js
@@ -1,27 +1,36 @@
 async function userLogged(ctx, next) {
-    if (ctx.state.currentUser) {
-        return next();
-    } else {
-        ctx.redirect(ctx.router.url('session.new'));
-    }
+  if (ctx.state.currentUser) {
+    return next();
+  }
+  ctx.redirect(ctx.router.url('session.new'));
 }
 
 async function checkProfileEditable(ctx, next) {
-    if (ctx.state.currentUser.id === parseInt(ctx.params.id, 10)) {
-        ctx.state.editableBoolean = true;
-        return next();
-    } else {
-        ctx.state.editableBoolean = false;
-        return next();
-    }
+  if (ctx.state.currentUser.id === parseInt(ctx.params.id, 10)) {
+    ctx.state.editableBoolean = true;
+    return next();
+  }
+  ctx.state.editableBoolean = false;
+  return next();
 }
 
 async function redirectIfNotUser(ctx, next) {
-    if (ctx.state.currentUser.id === parseInt(ctx.params.id, 10)) {
-        return next();
-    } else {
-        return ctx.redirect('/');
-    }
+  if (ctx.state.currentUser.id === parseInt(ctx.params.id, 10)) {
+    return next();
+  }
+  return ctx.redirect('/');
 }
 
-module.exports = { userLogged, checkProfileEditable, redirectIfNotUser };
+async function experienceGuard(ctx, next) {
+  const career = await ctx.orm.career.findById(ctx.params.id);
+  const currentUser = ctx.state.currentUser;
+
+  if (currentUser.universityId == career.universityId && currentUser.userType == 1) {
+    return next();
+  }
+  return ctx.redirect(ctx.router.url('careers.show', { id: career.id }));
+}
+
+module.exports = {
+  userLogged, checkProfileEditable, redirectIfNotUser, experienceGuard,
+};
