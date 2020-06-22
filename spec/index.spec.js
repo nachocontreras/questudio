851f5c993dcdf359a29769068251d13384aed76d
Luciano <lldavico@uc.cl>
2020-06-22 12:46:16 -0400

    changed port

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -2,6 +2,6 @@ const app = require('../src/app');
 
 console.log("Executing in " + process.env.NODE_ENV + " environment.")
 
-const PORT = process.env.PORT || 5432;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT)
 console.info(`Listening to ${PORT}`)
