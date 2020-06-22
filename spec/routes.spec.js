a572ee5add7d4e6f024843b8e56c323374a3403a
Luciano <lldavico@uc.cl>
2020-06-22 12:24:02 -0400

    travis host problem

--- spec/routes.spec.js
+++ spec/routes.spec.js
@@ -4,18 +4,12 @@ const app = require('../src/app')
 const cruds = [
     'universities',
     'careers',
-    'users',
     'vocacionales',
     'experiences',
     'team']
 
 cruds.forEach(async (crud) => {
-    var status;
-    if (crud == 'users') {
-        status = 500
-    } else {
-        status = 200
-    }
+    var status = 200;
     it(`${crud} route works`, async () => {
         const response = await request(app.callback()).get(`/${crud}`);
         expect(response.status).toBe(status);
