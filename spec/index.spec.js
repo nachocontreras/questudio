b90cc56da818d6e1b7d5b8c8e7312262a92329d8
Luciano <lldavico@uc.cl>
2020-06-22 11:26:05 -0400

    changed node environment

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -1,5 +1,7 @@
 const app = require('../src/app');
 
+console.log("Executing in " + process.env.NODE_ENV + " environment.")
+
 const PORT = process.env.PORT || 5432;
 app.listen(PORT)
 console.info(`Listening to ${PORT}`)
